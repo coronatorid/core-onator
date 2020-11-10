b79ad3e66c624310e9aea61014e11f98777d08a5
insomnius <aliana.arib@gmail.com>
2020-11-10 19:26:56 +0700

    Use mutex for concurrency safe

--- provider/infrastructure/infrastructure.go
+++ provider/infrastructure/infrastructure.go
@@ -24,12 +24,12 @@ func Fabricate() *Infrastructure {
 
 // MYSQL provide mysql interface
 func (i *Infrastructure) MYSQL() (*sql.DB, error) {
-	if i.mysqlDB != nil {
-		return i.mysqlDB, nil
-	}
+	var db *sql.DB
+	i.mysqlMutex.Do(func() {
+		// Currently there are no possible error while fabricating this so the error handling is ignored
+		db, _ = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&interpolateParams=true", os.Getenv("DATABASE_USERNAME"), os.Getenv("DATABASE_PASSWORD"), os.Getenv("DATABASE_HOST"), os.Getenv("DATABASE_PORT"), os.Getenv("DATABASE_NAME")))
+		i.mysqlDB = db
+	})
 
-	// Currently there are no possible error while fabricating this so the error handling is ignored
-	db, _ := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&interpolateParams=true", os.Getenv("DATABASE_USERNAME"), os.Getenv("DATABASE_PASSWORD"), os.Getenv("DATABASE_HOST"), os.Getenv("DATABASE_PORT"), os.Getenv("DATABASE_NAME")))
-	i.mysqlDB = db
-	return i.mysqlDB, nil
+	return db, nil
 }

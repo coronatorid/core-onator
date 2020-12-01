fb5c4711b29e49bc7d53e97dba8c4170f57a209b
insomnius <aliana.arib@gmail.com>
2020-12-01 21:21:13 +0700

    Change errorstring method output

--- entity/error.go
+++ entity/error.go
@@ -1,17 +1,28 @@
 package entity
 
+import "fmt"
+
 // ApplicationError cover all application error
 type ApplicationError struct {
 	Err        []error
 	HTTPStatus int
 }
 
+// APIError standard
+type APIError struct {
+	Status string `json:"status"`
+	Detail string `json:"detail"`
+}
+
 // ErrorString return error string as an array of string
-func (a *ApplicationError) ErrorString() []string {
-	appendedErrorString := []string{}
+func (a *ApplicationError) ErrorString() []APIError {
+	appendedErrorString := []APIError{}
 
 	for _, e := range a.Err {
-		appendedErrorString = append(appendedErrorString, e.Error())
+		appendedErrorString = append(appendedErrorString, APIError{
+			Detail: e.Error(),
+			Status: fmt.Sprintf("%d", a.HTTPStatus),
+		})
 	}
 
 	return appendedErrorString

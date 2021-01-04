ada2ad1a0bad86e7f8e51a3dccce2bdd9a891a25
insomnius <aliana.arib@gmail.com>
2021-01-04 22:02:14 +0700

    Add new method to create internal server error

---
+++ util/create_internal_server_error.go
@@ -0,0 +1,18 @@
+package util
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/coronatorid/core-onator/provider"
+
+	"github.com/coronatorid/core-onator/entity"
+)
+
+// CreateInternalServerError ...
+func CreateInternalServerError(ctx provider.Context) *entity.ApplicationError {
+	return &entity.ApplicationError{
+		Err:        []error{fmt.Errorf("Nampaknya terjadi kesalahan pada server coronator nih :(, sampaikan code ini pada twitter coronator.id ya biar di cek admin: %s", GetRequestID(ctx))},
+		HTTPStatus: http.StatusInternalServerError,
+	}
+}

7ee84cd55e7d5ddd21828dff38d49b34b3400fe1
insomnius <aliana.arib@gmail.com>
2021-01-12 22:31:16 +0700

    Request meta and response meta

---
+++ entity/api.go
@@ -0,0 +1,13 @@
+package entity
+
+// RequestMeta hold request information
+type RequestMeta struct {
+	Limit  int
+	Offset int
+}
+
+// ResponseMeta hold response information
+type ResponseMeta struct {
+	Limit  int
+	Offset int
+}

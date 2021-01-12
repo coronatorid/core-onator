4c3c555b8922cd2e1df828cd14154eb8516caa7b
insomnius <aliana.arib@gmail.com>
2021-01-12 22:39:31 +0700

    Better variable naming

--- provider/report/usecase/find_by_user_id.go
+++ provider/report/usecase/find_by_user_id.go
@@ -20,19 +20,19 @@ type FindByUserID struct{}
 
 // Perform find operation
 func (f *FindByUserID) Perform(ctx provider.Context, userID int, db provider.DB) (entity.ReportedCases, *entity.ApplicationError) {
-	var reportedCases entity.ReportedCases
+	var reportedCase entity.ReportedCases
 
 	row := db.QueryRowContext(ctx, "find-reported-cases", "select id, user_id, status, image_path, image_deleted, created_at, updated_at from reported_cases where user_id = ?", userID)
 	if err := row.Scan(
-		&reportedCases.ID,
-		&reportedCases.UserID,
-		&reportedCases.Status,
-		&reportedCases.ImagePath,
-		&reportedCases.ImageDeleted,
-		&reportedCases.CreatedAt,
-		&reportedCases.UpdatedAt,
+		&reportedCase.ID,
+		&reportedCase.UserID,
+		&reportedCase.Status,
+		&reportedCase.ImagePath,
+		&reportedCase.ImageDeleted,
+		&reportedCase.CreatedAt,
+		&reportedCase.UpdatedAt,
 	); err == provider.ErrDBNotFound {
-		return reportedCases, &entity.ApplicationError{
+		return reportedCase, &entity.ApplicationError{
 			Err:        []error{errors.New("reported cases not found")},
 			HTTPStatus: http.StatusNotFound,
 		}
@@ -43,15 +43,12 @@ func (f *FindByUserID) Perform(ctx provider.Context, userID int, db provider.DB)
 			Stack().
 			Array("tags", zerolog.Arr().Str("provider").Str("report").Str("find_by_user_id")).
 			Msg("failed find")
-		return reportedCases, &entity.ApplicationError{
-			Err:        []error{errors.New("service unavailable")},
-			HTTPStatus: http.StatusServiceUnavailable,
-		}
+		return reportedCase, util.CreateServiceUnavailable(ctx)
 	}
 
 	expiresAt := time.Now().Add(60 * time.Second).Unix()
-	signature := aurelia.Hash(os.Getenv("APP_ENCRIPTION_KEY"), fmt.Sprintf("%d%s", expiresAt, reportedCases.ImagePath))
-	reportedCases.ImagePath = fmt.Sprintf("%s%s?signature=%s&expires_at=%d", os.Getenv("API_HOST"), reportedCases.ImagePath, signature, expiresAt)
+	signature := aurelia.Hash(os.Getenv("APP_ENCRIPTION_KEY"), fmt.Sprintf("%d%s", expiresAt, reportedCase.ImagePath))
+	reportedCase.ImagePath = fmt.Sprintf("%s%s?signature=%s&expires_at=%d", os.Getenv("API_HOST"), reportedCase.ImagePath, signature, expiresAt)
 
-	return reportedCases, nil
+	return reportedCase, nil
 }
